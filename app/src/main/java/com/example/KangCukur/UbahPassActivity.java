14e0544cd9e43c13c2cae406d9006fb424297daa
shilnaprdisa <shilnaparadisa6@gmail.com>
2023-12-03 17:58:55 +0700

    first commit

---
+++ app/src/main/java/com/example/KangCukur/UbahPassActivity.java
@@ -0,0 +1,14 @@
+package com.example.kangcukur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UbahPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_pass);
+    }
+}
